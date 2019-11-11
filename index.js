d8ad063872139f221346faf8de25b2e91e06111c
phpdevroot <47479298+phpdevroot@users.noreply.github.com>
2019-11-11 20:39:40 -0300

    starting upload files

---
+++ index.js
@@ -0,0 +1,13 @@
+// Arquivo de script
+
+/*
+console.log('Ola Mundo!!');
+alert ("Ola Mundo!!!!  ");
+alert ('Buscando script em arquivos separados do HTML + Java Script');
+*/
+
+// Declarando variaveis
+
+var OlaMundo = "Ola Mundo!!!!!";
+alert (OlaMundo);
+console.log (OlaMundo)
